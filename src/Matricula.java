b9082d9ce819ec3081264fa713a8fe504cb28e72
Errygrajal <errygirigoyen@gmail.com>
2026-08-12 15:33:05 -0600

    Clase Matricula-Erry T Clase Estudiante modificada

--- src/Matricula.java
+++ src/Matricula.java
@@ -7,6 +7,23 @@
  *
  * @author UTN
  */
+import java.util.Date;
+
 public class Matricula {
-    
+    private EstudianteDeilyn estudiante;
+    private Date fecha;
+    private String estado;
+
+    public Matricula(EstudianteDeilyn estudiante) {
+        this.estudiante = estudiante;
+        this.fecha = new Date();
+        this.estado = "ACTIVA";
+    }
+
+    public void mostrarDetalle() {
+        System.out.println("=== Detalle de Matrícula ===");
+        System.out.println(estudiante.getDatos());
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Estado: " + estado);
+    }
 }
