fc8b2b97951f9fe84d15e3d6a2c589bc1a5c8e0f
Errygrajal <errygirigoyen@gmail.com>
2026-08-12 15:27:57 -0600

    Clase Matricula-Erry T

--- src/Matricula.java
+++ src/Matricula.java
@@ -2,11 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import java.util.Date;
 /**
  *
  * @author UTN
  */
 public class Matricula {
-    
+  
+    private EstudianteDeilyn estudiante;
+    private Date fecha;
+    private String estado;
+
+    public Matricula(EstudianteDeilyn estudiante) {
+        this.estudiante = estudiante;
+        this.fecha = new Date();
+        this.estado = "ACTIVA";
+    }
+
+    public void mostrarDetalle() {
+        System.out.println("Matrícula: " + estudiante.getDatos() + " | Fecha: " + fecha + " | Estado: " + estado);
+    }
+
 }
